dd1f1d830a197be664a93e403bff82d870f84bd1
Jackson Cai <jcai8649@gmail.com>
2020-09-29 00:47:30 -0700

    Create FB Header

---
+++ fb-clone/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="app">      
+
+      <Header />
+      
+      {/* App body */}
+
+      {/* Sidebar */}
+
+      {/* Feed */}
+
+      {/* Widgets */}
+    </div>
+  );
+}
+
+export default App;
---
+++ fb-clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDcUgvVJbSHwyN4m70p9Gdx2JcXmRA4emA",
+    authDomain: "facebook-clone-42163.firebaseapp.com",
+    databaseURL: "https://facebook-clone-42163.firebaseio.com",
+    projectId: "facebook-clone-42163",
+    storageBucket: "facebook-clone-42163.appspot.com",
+    messagingSenderId: "710300646078",
+    appId: "1:710300646078:web:33af1545b802ccd39dfcef",
+    measurementId: "G-ZMHZ8Z8S17"
+  };
